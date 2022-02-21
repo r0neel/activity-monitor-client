17bd6a4efbffc320014ee61d0bead6808b1a5a2f
Wai Lon <rxdvd0@gmail.com>
2022-02-21 10:22:06 +0000

    define handler functions

---
+++ static/js/handlers.js
@@ -0,0 +1,41 @@
+function loginSubmitHandler(e){
+
+}
+
+function registerSubmitHandler(e){
+
+}
+
+function formToggleHandler(e){
+
+}
+
+function navLinkHandler(e){
+
+}
+
+function habitClickHandler(e){
+
+}
+
+function newHabitClickHandler(e){
+
+}
+
+function habitUpdateHandler(e){
+
+}
+
+function habitSubmitHandler(e){
+
+}
+
+function habitDeleteBtnHandler(e){
+
+}
+
+module.exports = {
+    loginSubmitHandler, registerSubmitHandler, formToggleHandler,
+    navLinkHandler, habitClickHandler, newHabitClickHandler,
+    habitUpdateHandler, habitSubmitHandler, habitDeleteBtnHandler
+};
