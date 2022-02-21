cf82f61dcdeacfb7138494bfd82e9e27f1364801
Wai Lon <rxdvd0@gmail.com>
2022-02-21 10:11:16 +0000

    define render functions

---
+++ static/js/render.js
@@ -0,0 +1,27 @@
+function renderHome(){
+
+}
+
+function renderLoginForm(){
+
+}
+
+function renderRegisterForm(){
+
+}
+
+function renderNewHabitForm(){
+    
+}
+
+function renderHabitList(){
+
+}
+
+function renderHabitInfo(){
+
+}
+
+module.exports = {
+    renderHome, renderLoginForm, renderRegisterForm, renderNewHabitForm, renderHabitList, renderHabitInfo
+};
